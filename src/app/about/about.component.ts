bfe787f101b21c3fe578a0dc38111033ba9c7254
qdechery <qdechery@gmail.com>
2018-06-09 12:58:49 -0700

    'first'

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router) { 
+	this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  ngOnInit() {
+  }
+
+  sendMeHome(){
+  	this.router.navigate([''])
+  }
+
+}
